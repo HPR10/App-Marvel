56eaf2c60a4bd521ac357de557927ecb059dec18
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-03-25 23:28:34 -0300

    Initial Commit

---
+++ My App/My_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_AppApp.swift
+//  My App
+//
+//  Created by Hugo Pinheiro on 25/03/22.
+//
+
+import SwiftUI
+
+@main
+struct My_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
